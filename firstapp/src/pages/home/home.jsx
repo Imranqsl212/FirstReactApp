6098685443d618370fde081589a6220b53c5e444
Imranqsl212 <zakirovimran333@gmail.com>
2023-09-24 02:48:32 +0600

    added filter

--- firstapp/src/pages/home/home.jsx
+++ firstapp/src/pages/home/home.jsx
@@ -3,11 +3,26 @@ import axios from "axios";
 import CardList from "../../components/cards/cardList";
 import SearchInput from "../../components/UI/customSearchInput/SearchInput";
 import "./home.css";
+import Filter from "../../components/filter/Filter";
 
 const Home = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [searchQuery, setSearchQuery] = useState("");
+  const [activeCategoryIndex, setActiveCategoryIndex] = useState(0);
+
+  const handleCategoryChange = async (index) => {
+    const categoryParam = index === 0 ? "" : `?category=${index}`;
+
+    try {
+      const response = await axios.get(
+        `https://650efda754d18aabfe99b5f0.mockapi.io/api/cards${categoryParam}`
+      );
+      setData(response.data);
+    } catch (e) {
+      alert("Error: " + e);
+    }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,7 +31,7 @@ const Home = () => {
           "https://650efda754d18aabfe99b5f0.mockapi.io/api/cards"
         );
         setData(response.data);
-        setLoading(false)
+        setLoading(false);
 
         console.log(1);
       } catch (e) {
@@ -27,9 +42,14 @@ const Home = () => {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    handleCategoryChange(activeCategoryIndex);
+  }, [activeCategoryIndex]);
+
   const filteredData = data.filter((cardData) =>
     cardData.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
+
   return (
     <>
       <div className="cards">
@@ -38,6 +58,10 @@ const Home = () => {
           setSearchQuery={setSearchQuery}
         />
         <hr className="input__underline" />
+        <Filter
+          value={activeCategoryIndex}
+          setValue={(i) => setActiveCategoryIndex(i)}
+        />
         <h3 className="cards__h3">Рестораны в Москве</h3>
         <div className="cardWrapper">
           <CardList data={filteredData} loading={loading} />
